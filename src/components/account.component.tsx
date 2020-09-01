bf588605bc94e377ac167117c9d57356125f2dbb
Kareem Pierre <kareempierre@Kareems-MacBook-Pro.local>
2020-09-01 01:56:06 -0600

    fix padding

--- src/components/account.component.tsx
+++ src/components/account.component.tsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 function AccountComponent({ accountId, balance }: IAccount) {
     return (
-        <Link className="mb-8" to={`/account/${accountId}`}>
+        <Link className="mb-8 mr-4" to={`/account/${accountId}`}>
             <div className="bg-gray-300 w-84 max-w-xs h-48 shadow-md rounded p-8">
                 Account: {accountId}
-                <div className="text-3xl mt-8 ">Balance:</div>
+                <div className="text-3xl mt-8">Balance:</div>
                 <div className="text-xl truncate whitespace-no-wrap pl-2">&#36;{balance} CAD</div>
             </div>
         </Link>
--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -7,18 +7,18 @@ function Home() {
             <div className="flex justify-center bg-gray-300 pt-4 w-24">
                 HOME
             </div>
-            <div className="flex flex-col w-full h-screen p-10">
+            <div className="flex flex-col w-11/12 h-screen p-10">
                 <div className="flex flex-col justify-start h-half">
                     <div className="mb-4">Accounts:</div>
-                    <div className="flex flex-row flex-wrap justify-between overflow-scroll">
+                    <div className="flex flex-row justify-start overflow-scroll">
                         <AccountComponent accountId="1234" balance="50,000.00" />
                         <AccountComponent accountId="7474" balance="20,000.00" />
                         <AccountComponent accountId="7555" balance="25,000.00" />
                     </div>
                 </div>
-                <div className="flex flex-col flex-wrap justify-start h-half">
+                <div className="flex flex-col justify-start h-half">
                     <div className="mb-4">Joint Accounts:</div>
-                    <div className="flex flex-row justify-between">
+                    <div className="flex flex-row justify-start overflow-scroll">
                         <AccountComponent accountId="4567" balance="200.00" />
                         <AccountComponent accountId="8907" balance="1,500.00" />
                     </div>
