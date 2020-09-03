266794027aafc1100a747db60073829965314dae
Kareem Pierre <kareempierre@Kareems-MacBook-Pro.local>
2020-09-03 02:25:41 -0600

    add id for error component

--- src/components/error.component.tsx
+++ src/components/error.component.tsx
@@ -5,7 +5,7 @@ import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 function ErrorComponent({ message }: IError) {
     return (
-        <div className="flex fixed justify-center items-center h-screen w-screen z-30">
+        <div id="error-component" className="flex fixed justify-center items-center h-screen w-screen z-30">
             <div className="fixed h-screen w-screen bg-gray-900 bg-opacity-75 z-30"></div>
             <div className="flex flex-col w-40 h-40 rounded overflow-hidden shadow-lg bg-gray-900 z-40">
                 <div className="flex flex-1 justify-center items-center pt-1"><FontAwesomeIcon icon={faTimesCircle} color="red" size="3x"/></div>
